8866bbd1ea4fd4b70b2e6cdd563c07cead24b873
xiaoxiangyeyu-zxy <xiaoxiangyeyu@sjtu.edu.cn>
2023-03-14 19:30:33 +0800

    sss

---
+++ Helmholtz_read_interpolate_cpp/Helmholtz_read_interpolate/constant.h
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+#ifndef x_shape
+#define x_shape 29
+#endif
+#ifndef y_shape
+#define y_shape 21
+#endif
+#ifndef datanumber
+#define datanumber 2
+#endif
+#ifndef dlgE
+#define dlgE 0.5
+#endif
+#ifndef dlgDen
+#define dlgDen 0.5
+#endif
+#ifndef lgEmin 
+#define lgEmin 16.
+#endif
+#ifndef lgEmax
+#define lgEmax 26.
+#endif
+#ifndef lgDenmin 
+#define lgDenmin -10.
+#endif
+#ifndef lgDenmax
+#define lgDenmax 4.
+#endif
